56eb441a444e8911945138d1b516ec2145f11592
Alicia-Natasha <alicia.natasha@student.moringaschool.com>
2025-05-12 15:31:17 +0300

    python-p3-ineritance-lab

--- lib/teacher.py
+++ lib/teacher.py
@@ -6,5 +6,14 @@ import random
 
 class Teacher(User):
 
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.knowledge = ["Python basics", "OOP concepts"]
+
     def teach(self):
-        pass
+        '''returns a random knowledge item.'''
+        return random.choice(self.knowledge)
+    
+    
+
